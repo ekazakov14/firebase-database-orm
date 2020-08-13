0b5c6a8fc29cbee36b1992cfa99c8bc1cc0ecca4
eugene <eugene.kazakov78@gmail.com>
2020-08-14 00:56:50 +0300

    add not found error handler in FileModel

--- src/File.ts
+++ src/File.ts
@@ -1,6 +1,7 @@
 import firebase from 'firebase';
 import 'firebase/storage';
 import { v4 } from 'uuid';
+import { STORAGE_OBJECT_NOT_FOUND } from '@constants/error';
 
 class FileModel {
   constructor(protected file: File) {
@@ -10,14 +11,33 @@ class FileModel {
     const metaPromise = this.child(id).getMetadata();
     const urlPromise = this.getUrl(id);
 
-    return Promise.all([metaPromise, urlPromise]).then(([meta, url]) => ({
-      ...meta,
-      url,
-    }));
+    try {
+      const [meta, url] = await Promise.all([metaPromise, urlPromise]);
+      return {
+        ...meta,
+        url,
+      };
+    } catch (error) {
+      // eslint-disable-next-line no-underscore-dangle
+      if (error.code_ === STORAGE_OBJECT_NOT_FOUND) {
+        return null;
+      }
+
+      throw error;
+    }
   }
 
-  public static async getUrl(id: string): Promise<string> {
-    return this.child(id).getDownloadURL();
+  public static async getUrl(id: string): Promise<string|null> {
+    try {
+      return await this.child(id).getDownloadURL();
+    } catch (error) {
+      // eslint-disable-next-line no-underscore-dangle
+      if (error.code_ === STORAGE_OBJECT_NOT_FOUND) {
+        return null;
+      }
+
+      throw error;
+    }
   }
 
   public async save(id: string = v4()): Promise<string> {
--- src/constants/error.ts
+++ src/constants/error.ts
@@ -1,7 +1,9 @@
 const WRONG_PROPERTIES = 'Wrong properties, define all fields by decorator';
 const EMPTY_FIELDS_MODEL = 'No one field in model';
+const STORAGE_OBJECT_NOT_FOUND = 'storage/object-not-found';
 
 export {
   WRONG_PROPERTIES,
   EMPTY_FIELDS_MODEL,
+  STORAGE_OBJECT_NOT_FOUND,
 };
